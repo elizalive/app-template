7679a613696c0c4a7f121bf3da68f837702433ab
Elizabeth <elizabeth.scheltens@gmail.com>
2015-05-07 15:15:15 -0400

    set defaults
    set defaults to decodedc rather than npr

--- app_config.py
+++ app_config.py
@@ -26,7 +26,7 @@ PROJECT_FILENAME = '$NEW_PROJECT_FILENAME'
 
 # The name of the repository containing the source
 REPOSITORY_NAME = '$NEW_REPOSITORY_NAME'
-GITHUB_USERNAME = 'nprapps'
+GITHUB_USERNAME = 'elizalive'
 REPOSITORY_URL = 'git@github.com:%s/%s.git' % (GITHUB_USERNAME, REPOSITORY_NAME)
 REPOSITORY_ALT_URL = None # 'git@bitbucket.org:nprapps/%s.git' % REPOSITORY_NAME'
 
@@ -38,24 +38,24 @@ ASSETS_SLUG = '$NEW_PROJECT_SLUG'
 DEPLOYMENT
 """
 PRODUCTION_S3_BUCKET = {
-    'bucket_name': 'apps.npr.org',
+    'bucket_name': 'apps.decodedc.com',
     'region': 'us-east-1'
 }
 
 STAGING_S3_BUCKET = {
-    'bucket_name': 'stage-apps.npr.org',
+    'bucket_name': 'stage-apps.decodedc.com',
     'region': 'us-east-1'
 }
 
 ASSETS_S3_BUCKET = {
-    'bucket_name': 'assets.apps.npr.org',
+    'bucket_name': 'assets.apps.decodedc.com',
     'region': 'us-east-1'
 }
 
 DEFAULT_MAX_AGE = 20
 
-PRODUCTION_SERVERS = ['cron.nprapps.org']
-STAGING_SERVERS = ['cron-staging.nprapps.org']
+# PRODUCTION_SERVERS = ['cron.nprapps.org']
+# STAGING_SERVERS = ['cron-staging.nprapps.org']
 
 # Should code be deployed to the web/cron servers?
 DEPLOY_TO_SERVERS = False
@@ -97,7 +97,7 @@ DEBUG = True
 """
 COPY EDITING
 """
-COPY_GOOGLE_DOC_KEY = '0AlXMOHKxzQVRdHZuX1UycXplRlBfLVB0UVNldHJYZmc'
+COPY_GOOGLE_DOC_KEY = '0Ar2zsmBcYUIudE5zYkJ5RFNjZmt5R1ZUQy1ObkxTUHc'
 COPY_PATH = 'data/copy.xlsx'
 
 """
@@ -119,18 +119,18 @@ NPR_DFP = {
 """
 SERVICES
 """
-NPR_GOOGLE_ANALYTICS = {
-    'ACCOUNT_ID': 'UA-5828686-4',
-    'DOMAIN': PRODUCTION_S3_BUCKET['bucket_name'],
-    'TOPICS': '' # e.g. '[1014,3,1003,1002,1001]'
-}
+# NPR_GOOGLE_ANALYTICS = {
+#     'ACCOUNT_ID': 'UA-5828686-4',
+#     'DOMAIN': PRODUCTION_S3_BUCKET['bucket_name'],
+#     'TOPICS': '' # e.g. '[1014,3,1003,1002,1001]'
+# }
 
-VIZ_GOOGLE_ANALYTICS = {
-    'ACCOUNT_ID': 'UA-5828686-75'
-}
+# VIZ_GOOGLE_ANALYTICS = {
+#     'ACCOUNT_ID': 'UA-5828686-75'
+# }
 
-DISQUS_API_KEY = 'tIbSzEhGBE9NIptbnQWn4wy1gZ546CsQ2IHHtxJiYAceyyPoAkDkVnQfCifmCaQW'
-DISQUS_UUID = '$NEW_DISQUS_UUID'
+# DISQUS_API_KEY = 'tIbSzEhGBE9NIptbnQWn4wy1gZ546CsQ2IHHtxJiYAceyyPoAkDkVnQfCifmCaQW'
+# DISQUS_UUID = '$NEW_DISQUS_UUID'
 
 """
 OAUTH
